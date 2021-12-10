b3741ccdf810a86eba5e0c356a78fe3fdd57d99d
HenryShan <zshan2@illinois.edu>
2021-12-09 18:06:28 -0600

    add receiver2.py

---
+++ receiver2.py
@@ -0,0 +1,13 @@
+import sys
+import RPi.GPIO as GPIO
+import time
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(4, GPIO.OUT)
+while True:
+    GPIO.output(4, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(4, GPIO.LOW)
+    time.sleep(0.5)
