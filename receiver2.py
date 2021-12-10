63b12aa92141cdc2a6189092c0b693d5bedf82af
HenryShan <zshan2@illinois.edu>
2021-12-09 18:24:44 -0600

    update 1.1

--- receiver2.py
+++ receiver2.py
@@ -5,9 +5,24 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(4, GPIO.OUT)
-while True:
-    GPIO.output(4, GPIO.HIGH)
+
+clk = 4
+b0 = 17
+b1 = 27
+b2 = 22
+b3 = 23
+
+GPIO.setup(clk, GPIO.OUT)
+GPIO.setup(b0, GPIO.IN)
+GPIO.setup(b1, GPIO.IN)
+GPIO.setup(b2, GPIO.IN)
+GPIO.setup(b3, GPIO.IN)
+
+for n in range(100000):
+    GPIO.output(clk, GPIO.HIGH)
     time.sleep(0.5)
-    GPIO.output(4, GPIO.LOW)
+    print(b3 << 3 | b2 << 2 | b1 << 1 | b0)
+    GPIO.output(clk, GPIO.LOW)
+    print(b3 << 3 | b2 << 2 | b1 << 1 | b0)
     time.sleep(0.5)
+    
