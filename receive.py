7dfd52a0ecdf32ece4d31ecc19894c92ecca0779
Jiacheng Huang <huangjiacheng0709@outlook.com>
2021-12-10 15:29:26 -0600

    Add library for ADC0820

--- receive.py
+++ receive.py
@@ -1,8 +1,11 @@
-from gpiozero import SmoothedInputDevice
-from time import sleep
+from adc0820 import ADC0820
 
-light_sensor = SmoothedInputDevice(18)
+receiver = ADC0820(
+    db0=17, db1=27, db2=22, db3=23,
+    db4=24, db5=25, db6=5, db7=6,
+    rd_n=18, int_n=19, cs_n=20, ofl_n=21,
+    wr_n=16, mode=26
+)
 
 while True:
-    print(light_sensor.value)
-    sleep(0.1)
+    print(receiver.read())
