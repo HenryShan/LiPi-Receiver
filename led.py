f10363ecd842a519dfb8d802d84ba9f5d1a05c3e
HenryShan <zshan2@illinois.edu>
2021-12-09 20:17:20 -0600

    Create led.py

---
+++ led.py
@@ -0,0 +1,15 @@
+import sys
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+clk = 5
+GPIO.setup(clk, GPIO.OUT)
+
+for n in range(100000):
+    GPIO.output(clk, GPIO.HIGH)
+    time.sleep(0.1)
+    GPIO.output(clk, GPIO.LOW)
+    time.sleep(0.1)
