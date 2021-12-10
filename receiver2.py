2d9ba12a07173ffb958128b6b080fce7216f44ee
HenryShan <zshan2@illinois.edu>
2021-12-09 19:24:52 -0600

    Update receiver2.py

--- receiver2.py
+++ receiver2.py
@@ -2,8 +2,18 @@ import sys
 import RPi.GPIO as GPIO
 import time
 
-def get_gpio_value(b0, b1, b2, b3):
-    return (GPIO.input(b3) << 3 | GPIO.input(b2) << 2 | GPIO.input(b1) << 1 | GPIO.input(b0))
+def get_gpio_value(b0, b1, b2, b3, b4, b5, b6, b7):
+    return (
+        GPIO.input(b7) << 7 |
+        GPIO.input(b6) << 6 |
+        GPIO.input(b5) << 5 |
+        GPIO.input(b4) << 4 |
+        GPIO.input(b3) << 3 |
+        GPIO.input(b3) << 3 | 
+        GPIO.input(b2) << 2 | 
+        GPIO.input(b1) << 1 | 
+        GPIO.input(b0)
+    )
 
 
 GPIO.setmode(GPIO.BCM)
@@ -14,6 +24,10 @@ b0 = 17
 b1 = 27
 b2 = 22
 b3 = 23
+b4 = 14
+b5 = 15
+b6 = 18
+b7 = 24
 
 GPIO.setup(clk, GPIO.OUT)
 GPIO.setup(b0, GPIO.IN)
@@ -23,9 +37,9 @@ GPIO.setup(b3, GPIO.IN)
 
 for n in range(100000):
     GPIO.output(clk, GPIO.HIGH)
-    print(get_gpio_value(b0, b1, b2, b3))
+    print(get_gpio_value(b0, b1, b2, b3, b4, b5, b6, b7))
     time.sleep(0.2)
     GPIO.output(clk, GPIO.LOW)
-    print(get_gpio_value(b0, b1, b2, b3))
+    print(get_gpio_value(b0, b1, b2, b3, b4, b5, b6, b7))
     time.sleep(0.2)
     
